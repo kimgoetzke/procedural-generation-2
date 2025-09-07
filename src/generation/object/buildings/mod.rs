dc547bde525f76172afdfc85f8980eecb60e914b
Kim Goetzke <kbgoetzke@gmail.com>
2025-09-07 09:25:04 +0100

    Spawn buildings on single, edge connection points

--- src/generation/object/buildings/mod.rs
+++ src/generation/object/buildings/mod.rs
@@ -30,7 +30,12 @@ struct BuildingTemplate {
 }
 
 impl BuildingTemplate {
-  pub fn new(name: &str, tiles: Vec<Vec<ObjectName>>, door_x: i32, door_y: i32, connection_direction: Direction) -> Self {
+  pub fn new(
+    name: &str,
+    tiles: Vec<Vec<ObjectName>>,
+    relative_door_ig: Point<InternalGrid>,
+    connection_direction: Direction,
+  ) -> Self {
     let height = tiles.len() as i32;
     let width = if height > 0 { tiles[0].len() as i32 } else { 0 };
 
@@ -39,15 +44,15 @@ impl BuildingTemplate {
       width,
       height,
       tiles,
-      relative_door_ig: Point::new_internal_grid(door_x, door_y),
+      relative_door_ig,
       connection_direction,
     }
   }
 
-  /// Calculates where the building's top-left corner should be placed given a connection point.
-  fn calculate_building_top_left_ig(&self, connection_point: Point<InternalGrid>) -> Point<InternalGrid> {
-    // Calculate where the door should be (one tile away from connection point in the opposite direction)
-    let door_ig = match self.connection_direction {
+  /// Calculates where the building's top-left corner should be placed given a connection point which is one tile away
+  /// from connection point in the opposite direction.
+  fn calculate_building_origin_ig(&self, connection_point: Point<InternalGrid>) -> Point<InternalGrid> {
+    let absolute_door_ig = match self.connection_direction {
       Direction::Top => Point::new_internal_grid(connection_point.x, connection_point.y + 1),
       Direction::Bottom => Point::new_internal_grid(connection_point.x, connection_point.y - 1),
       Direction::Left => Point::new_internal_grid(connection_point.x + 1, connection_point.y),
@@ -55,7 +60,10 @@ impl BuildingTemplate {
       _ => panic!("Invalid connection direction for building template"),
     };
 
-    Point::new_internal_grid(door_ig.x - self.relative_door_ig.x, door_ig.y - self.relative_door_ig.y)
+    Point::new_internal_grid(
+      absolute_door_ig.x - self.relative_door_ig.x,
+      absolute_door_ig.y - self.relative_door_ig.y,
+    )
   }
 
   fn can_place_at_connection(
@@ -63,47 +71,43 @@ impl BuildingTemplate {
     connection_point: Point<InternalGrid>,
     available_space: &HashSet<Point<InternalGrid>>,
   ) -> bool {
-    let building_origin = self.calculate_building_top_left_ig(connection_point);
+    let building_origin_ig = self.calculate_building_origin_ig(connection_point);
 
-    // Check chunk boundaries
-    if building_origin.x < 0
-      || building_origin.y < 0
-      || building_origin.x + self.width > CHUNK_SIZE
-      || building_origin.y + self.height > CHUNK_SIZE
+    // Don't allow buildings to be placed out of bounds
+    if building_origin_ig.x < 0
+      || building_origin_ig.y < 0
+      || building_origin_ig.x + self.width > CHUNK_SIZE
+      || building_origin_ig.y + self.height > CHUNK_SIZE
     {
       return false;
     }
 
-    // Check if all building tiles are available
+    // Make sure all tiles the building will occupy are available
     for y in 0..self.height {
       for x in 0..self.width {
-        let tile_pos = Point::new_internal_grid(building_origin.x + x, building_origin.y + y);
-        if !available_space.contains(&tile_pos) {
+        let tile_ig = Point::new_internal_grid(building_origin_ig.x + x, building_origin_ig.y + y);
+        if !available_space.contains(&tile_ig) {
           return false;
         }
       }
     }
 
-    // Verify the connection point is adjacent to where the door will be
-    let door_world_position = Point::new_internal_grid(
-      building_origin.x + self.relative_door_ig.x,
-      building_origin.y + self.relative_door_ig.y,
+    let door_ig = Point::new_internal_grid(
+      building_origin_ig.x + self.relative_door_ig.x,
+      building_origin_ig.y + self.relative_door_ig.y,
+    );
+    let connection_point_direction: Point<InternalGrid> = self.connection_direction.to_opposite().to_point();
+    let expected_door_ig = Point::new_internal_grid(
+      connection_point.x + connection_point_direction.x,
+      connection_point.y + connection_point_direction.y,
     );
 
-    self.is_adjacent(door_world_position, connection_point)
-  }
-
-  fn is_adjacent(&self, reference_ig: Point<InternalGrid>, other_ig: Point<InternalGrid>) -> bool {
-    let dx = (reference_ig.x - other_ig.x).abs();
-    let dy = (reference_ig.y - other_ig.y).abs();
-
-    (dx == 1 && dy == 0) || (dx == 0 && dy == 1)
+    door_ig == expected_door_ig
   }
 }
 
 // TODO: Override path tiles in front of doors to ensure seamless connectivity
 // TODO: Make sure buildings face the correct direction based on path direction
-// TODO: Make it possible for buildings to spawn at single edge connection points with no connections
 /// The entry point for determining buildings in the object grid.
 pub fn place_buildings_on_grid(object_grid: &mut ObjectGrid, settings: &Settings, metadata: &Metadata, rng: &mut StdRng) {
   let start_time = shared::get_time();
@@ -112,12 +116,14 @@ pub fn place_buildings_on_grid(object_grid: &mut ObjectGrid, settings: &Settings
     debug!("Skipped generating buildings for {} because path generation is disabled", cg);
     return;
   }
-  let connection_points = metadata
-    .get_connection_points_for(&cg, object_grid)
-    .iter_mut()
-    .filter(|cp| !cp.is_touching_edge())
-    .map(|cp| *cp)
-    .collect::<Vec<Point<InternalGrid>>>();
+  let mut connection_points = metadata.get_connection_points_for(&cg, object_grid);
+  if connection_points.len() > 1 {
+    connection_points = connection_points
+      .iter_mut()
+      .filter(|cp| !cp.is_touching_edge())
+      .map(|cp| *cp)
+      .collect::<Vec<Point<InternalGrid>>>();
+  }
   if connection_points.is_empty() {
     debug!(
       "No internal connection points found for {} - skipping building generation",
@@ -126,29 +132,29 @@ pub fn place_buildings_on_grid(object_grid: &mut ObjectGrid, settings: &Settings
     return;
   }
 
-  let available_space = compute_available_space_map(object_grid);
   let building_templates = get_building_templates();
+  let available_grid_space = compute_available_space_map(object_grid);
+  let mut occupied_grid_space = HashSet::new();
   let mut buildings_placed = 0;
-  let mut occupied_positions = HashSet::new();
   for connection_point in connection_points {
     if let Some(template) = select_fitting_building(
       &building_templates,
       connection_point,
-      &available_space,
-      &occupied_positions,
+      &available_grid_space,
+      &occupied_grid_space,
       rng,
     ) {
-      let building_origin = template.calculate_building_top_left_ig(connection_point);
-      if place_building(object_grid, &template, building_origin, &mut occupied_positions) {
+      let building_origin_ig = template.calculate_building_origin_ig(connection_point);
+      if place_building(&template, building_origin_ig, object_grid, &mut occupied_grid_space) {
         buildings_placed += 1;
         debug!(
           "Placed [{}] at computed origin {:?} for connection point {:?} on {}",
-          template.name, building_origin, connection_point, cg
+          template.name, building_origin_ig, connection_point, cg
         );
       } else {
         warn!(
           "Failed to place [{}] at computed origin {:?} on {}",
-          template.name, building_origin, cg
+          template.name, building_origin_ig, cg
         );
       }
     } else {
@@ -192,14 +198,14 @@ fn select_fitting_building(
   occupied_space: &HashSet<Point<InternalGrid>>,
   rng: &mut StdRng,
 ) -> Option<BuildingTemplate> {
-  let mut fitting_templates = Vec::new();
+  let mut fitting_building_templates = Vec::new();
   for template in templates {
     if template.can_place_at_connection(connection_point, available_space) {
-      let building_origin = template.calculate_building_top_left_ig(connection_point);
+      let origin_ig = template.calculate_building_origin_ig(connection_point);
       let mut is_overlapping = false;
       for y in 0..template.height {
         for x in 0..template.width {
-          let ig = Point::new_internal_grid(building_origin.x + x, building_origin.y + y);
+          let ig = Point::new_internal_grid(origin_ig.x + x, origin_ig.y + y);
           if occupied_space.contains(&ig) {
             is_overlapping = true;
             break;
@@ -210,35 +216,36 @@ fn select_fitting_building(
         }
       }
       if !is_overlapping {
-        fitting_templates.push(template.clone());
+        fitting_building_templates.push(template.clone());
       }
     }
   }
-
-  if fitting_templates.is_empty() {
+  if fitting_building_templates.is_empty() {
     return None;
   }
+  let index = rng.random_range(0..fitting_building_templates.len());
 
-  let selected_index = rng.random_range(0..fitting_templates.len());
-
-  Some(fitting_templates[selected_index].clone())
+  Some(fitting_building_templates[index].clone())
 }
 
 fn place_building(
-  object_grid: &mut ObjectGrid,
   template: &BuildingTemplate,
-  building_origin: Point<InternalGrid>,
-  occupied_positions: &mut HashSet<Point<InternalGrid>>,
+  building_origin_ig: Point<InternalGrid>,
+  object_grid: &mut ObjectGrid,
+  occupied_space: &mut HashSet<Point<InternalGrid>>,
 ) -> bool {
   for y in 0..template.height {
     for x in 0..template.width {
-      let ig = Point::new_internal_grid(building_origin.x + x, building_origin.y + y);
+      let ig = Point::new_internal_grid(building_origin_ig.x + x, building_origin_ig.y + y);
       let object_name = template.tiles[y as usize][x as usize];
       if let Some(cell) = object_grid.get_cell_mut(&ig) {
         cell.mark_as_collapsed(object_name);
-        occupied_positions.insert(ig);
+        occupied_space.insert(ig);
       } else {
-        error!("Failed to get cell at {:?} for building placement", ig);
+        error!(
+          "Failed to get cell at {:?} for building placement on object grid for {}",
+          ig, object_grid.cg
+        );
         return false;
       }
     }
@@ -264,8 +271,7 @@ fn get_building_templates() -> Vec<BuildingTemplate> {
           ObjectName::HouseSmallWallRight,
         ],
       ],
-      1,
-      1,
+      Point::new_internal_grid(1, 1),
       Direction::Bottom,
     ),
     BuildingTemplate::new(
@@ -282,8 +288,7 @@ fn get_building_templates() -> Vec<BuildingTemplate> {
           ObjectName::HouseSmallWallRight,
         ],
       ],
-      1,
-      0,
+      Point::new_internal_grid(1, 0),
       Direction::Left,
     ),
     BuildingTemplate::new(
@@ -300,8 +305,7 @@ fn get_building_templates() -> Vec<BuildingTemplate> {
           ObjectName::HouseSmallDoorRight,
         ],
       ],
-      1,
-      2,
+      Point::new_internal_grid(1, 2),
       Direction::Right,
     ),
     BuildingTemplate::new(
@@ -318,8 +322,7 @@ fn get_building_templates() -> Vec<BuildingTemplate> {
           ObjectName::HouseMediumWallRight,
         ],
       ],
-      1,
-      1,
+      Point::new_internal_grid(1, 1),
       Direction::Bottom,
     ),
   ]
